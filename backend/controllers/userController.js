f7156309c4d16463bc4729e2cee1aa1d66692304
rhey_yu <rhey2476@gmail.com>
2023-10-05 21:46:28 -0800

    modified route and user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,38 @@
-const authUser = (req, res) => {
+import expressAsyncHandler from "express-async-handler";
+
+
+//authenticate user
+const authUser = expressAsyncHandler(async(req, res) => {
+    //checking custom error    
+    // res.status(401);
+    // throw new Error('something went hywire!');
     res.status(200).json({message: 'Auth User'})
+});
+//register new account
+const registerUser = expressAsyncHandler(async(req, res) => {
+
+    res.status(200).json({message: 'Register User'})
+});
+//logout account
+const logoutUser = expressAsyncHandler(async(req, res) => {
+
+    res.status(200).json({message: 'Logout User'})
+});
+//user profile account
+const getUserProfile = expressAsyncHandler(async(req, res) => {
+
+    res.status(200).json({message: 'User Profile'})
+});
+//update user account
+const updateUserProfile = expressAsyncHandler(async(req, res) => {
+
+    res.status(200).json({message: 'User Profile Update'})
+});
+
+export {
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
 };
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import Express from "express";
 import dotenv from "dotenv";
-
-dotenv.config();
-const app = Express();
 const port = process.env.PORT || 5000;
+import userRoutes from "./routes/userRoutes.js";
+const app = Express();
+dotenv.config();
+import { requestNotFound, errorHandler } from "./middleware/customError.js";
 
+app.use('/users', userRoutes);
 app.get('/', (req, res) => res.send('Server is ready'));
+app.use(requestNotFound);
+app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server is active on port ${port}`));
+app.listen(port, () => console.log(`Server is active on port=> ${port}`));
